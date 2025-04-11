b89ad1f0d5537e4bab1d8e49662482a223bafadd
HuntOreo <hloreair@outlook.com>
2025-04-10 20:00:15 -0400

    added a landing page to the app. call it dynamic rendering

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ Swaps teams after each play
 Can reset the board
 Keeps track of score per game
 Updates the scoreboard
+Landing page that allows you to start a game
 
 ******* Doing **********
 Game UI
 
 ******* To-Do **********
+Store cookie that keeps track of a player's score
+Render Player score on landing page when site is loaded
 Button to wipe score and start a new game
 Add best of 3 and 5
 Special effect when winning move
@@ -24,6 +27,7 @@ Online Multiplayer function
 3+ player functionionality
 */
 
+import { state } from './state.js'
 import {
   clickedMe,
   reset,
@@ -42,8 +46,58 @@ tile.forEach(el => {
 const resetBtn = document.querySelector('#reset')
 resetBtn.onclick = reset
 
-window.addEventListener('resize', () => {
-  document.body.style.display = 'none';
-  document.body.offsetHeight; // force reflow
-  document.body.style.display = '';
-});
+// Will hide the landing page and show the board game
+const landingPage = document.querySelector('.landing-page')
+const boardPage = document.querySelector('.board-page')
+
+const startGameBtn = document.querySelector('.startGame')
+const loadBoardPage = () =>{
+  
+
+  boardPage.classList.remove('hidePage')
+  landingPage.classList.add('hidePage')
+}
+
+// Will hide the landing page and show the board game
+const endGameBtn = document.querySelector('#endGame')
+const loadLandingPage = () =>{
+
+  wipeSlate()
+  landingPage.classList.remove('hidePage')
+  boardPage.classList.add('hidePage')
+}
+
+
+const wipeSlate = () => {
+
+  let count = 0
+  const scores = document.querySelectorAll('.score')
+
+  state.turn = 'X'
+  state.players.forEach((player)=> {
+    player.score = 0
+    if (count < state.players.length) {
+      scores[count].innerHTML = ""
+      scores[count].innerHTML = `
+          <div class="scoreWindow">
+            <p>${player.name} Score:</p>
+            <p class="tally">${player.score}</p>
+          </div>
+        `
+      count++
+    }
+  })
+
+  reset()
+
+}
+
+
+// window.addEventListener('resize', () => {
+//   document.body.style.display = 'none';
+//   document.body.offsetHeight; // force reflow
+//   document.body.style.display = '';
+// });
+
+startGameBtn.onclick = loadBoardPage
+endGameBtn.onclick = loadLandingPage
